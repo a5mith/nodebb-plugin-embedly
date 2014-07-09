5a18da1d166e464b0e9df7832c1ffaac3d448cfb
a5mith <aaronsmith91@sky.com>
2014-07-09 16:43:13 +0100

    Embed.ly
    0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB HearThis Plugin
+
+This NodeBB plugin creates cards for URLs inside users posts.
+
+## Installation
+
+    npm install nodebb-plugin-embedly
+
+## Usage
+
+    Insert a URL, embed.ly will make it into a card with more info.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var Embedly = {},
+        embed = '<a class="embedly-card" href="http://35hz.co.uk"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>';
+
+
+    Embedly.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = Embedly;
+}(module));
