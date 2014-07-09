f023f6cbbcb340cf1ea978c9a29d263dc96a09c7
a5mith <aaronsmith91@sky.com>
2014-07-09 16:46:09 +0100

    D'oh.

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
 
     var Embedly = {},
-        embed = '<a class="embedly-card" href="http://35hz.co.uk"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>';
+        embed = '<a class="embedly-card" href="$1"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>';
 
 
     Embedly.parse = function(postContent, callback) {
