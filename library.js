071716213e3becf0d2080b64eb0507015b07b0c1
a5mith <aaronsmith91@sky.com>
2014-11-25 17:59:45 +0000

    0.6.0 Compat.

--- library.js
+++ library.js
@@ -3,11 +3,17 @@
 
     var Embedly = {},
         embed = '<a class="embedly-card" href="$1"></a><script>!function(a){var b="embedly-platform",c="script";if(!a.getElementById(b)){var d=a.createElement(c);d.id=b,d.src=("https:"===document.location.protocol?"https":"http")+"://cdn.embedly.com/widgets/platform.js";var e=document.getElementsByTagName(c)[0];e.parentNode.insertBefore(d,e)}}(document);</script>';
+    var embedlycode = '/<a href="(.+)">.+<\/a>/g';
 
+    Embedly.parse = function(data, callback) {
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(embedlycode)) {
+            data.postData.content = data.postData.content.replace(embedlycode, embed);
+        }
+        callback(null, data);
 
-    Embedly.parse = function(postContent, callback) {
-        postContent = postContent.replace(/<a href="(.+)">.+<\/a>/g, embed);
-        callback(null, postContent);
     };
 
     module.exports = Embedly;
